4ce54265af046f67012bf45572cd5d7b184aba88
MichaelSchffl <56416984+MichaelSchffl@users.noreply.github.com>
2020-10-17 22:49:46 +0200

    update images

--- Darcy.py
+++ Darcy.py
@@ -9,5 +9,6 @@ def solveDarcy(mesh, K, bc):
     h = pg.solve(mesh, a = K, bc = bc) #m
     #Return the discrete interpolated gradient u for a given scalar field h.
     fig,ax = plt.subplots(1,1,figsize=(17, 11.5))
+    ax.tick_params(labelsize=15)
     pg.show(mesh, h, ax=ax, label="hydraulic head distribution h [m]", nLevs= 9)
     return(h)
--- StreamPotential.py
+++ StreamPotential.py
@@ -8,7 +8,8 @@ def solveStreampot(mesh, sigma, bc, f):
     #u = streaming potential phi
     phi = pg.solve(mesh, a = sigma, f = f, bc = bc)
     E = pg.solver.grad(mesh, -phi)
-    fig, axes = plt.subplots(figsize =(16,8))
+    fig, axes = plt.subplots(figsize =(17,11.5))
+    axes.tick_params(labelsize=15)
     #pg.show(mesh, E, ax = axes, hold=True)
     #mt.nodeDataToCellData(mesh1, phi)
     pg.show(mesh, phi*1000, label = r"streaming potential " r"$\varphi$ [mV]", ax=axes, cMap = 'RdBu_r', nLevs = 11)
