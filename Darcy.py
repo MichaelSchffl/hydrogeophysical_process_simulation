8f23d9c7295670e2ace73fcb6573d8dd16bc4a32
MichaelSchffl <56416984+MichaelSchffl@users.noreply.github.com>
2020-10-25 11:43:07 +0100

    darcy

--- Darcy.py
+++ Darcy.py
@@ -8,8 +8,7 @@ def solveDarcy(mesh, K, bc):
     #u = h (hydraulic head), v = q (specific flux), differential equation:
     h = pg.solve(mesh, a = K, bc = bc) #m
     #Return the discrete interpolated gradient u for a given scalar field h.
-    fig,ax = plt.subplots()
+    fig,ax = plt.subplots(figsize=(17,11.5))
     ax.tick_params(labelsize=15)
     pg.show(mesh, h, ax=ax, label="hydraulic head distribution h [m]", nLevs= 9)
-    fig.savefig('images/hydr_grad2.png', bbox_inches='tight')
     return(h)
